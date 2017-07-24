e9ee8cf73e474ecba459f3e84d69966c2118295b
cst13 <30401335+cst13@users.noreply.github.com>
2017-07-24 13:48:19 +0700

    Add files via upload

---
+++ do_release.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import sys
+import argparse
+import pkg_resources
+
+from moneywagon import service_table
+from moneywagon.supply_estimator import write_blocktime_adjustments
+
+from subprocess import call
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--minor', action='store_true', help='Make minor release.')
+parser.add_argument('--major', action='store_true', help='Make major release.')
+parser.add_argument('--point', action='store_true', help='Make point release.')
+parser.add_argument('--skip-blocktime-adjustments', default=True, action='store_true', help='Skip calculating blocktime adjustments')
+
+argz = parser.parse_args()
+
+if not argz.skip_blocktime_adjustments:
+    import debug
+    print "Writing new bocktime adjustments..."
+    write_blocktime_adjustments("moneywagon/blocktime_adjustments.py", random=True, verbose=True)
+
+ex_major, ex_minor, ex_point = pkg_resources.get_distribution('moneywagon').version.split(".")
+version = "%s.%s.%s"
+
+if argz.major:
+    version = version % (int(ex_major) + 1, "0", "0")
+elif argz.minor:
+    version = version % (ex_major, int(ex_minor) + 1, "0")
+elif argz.point:
+    version = version % (ex_major, ex_minor, int(ex_point) + 1)
+
+with open("setup.py", 'w') as f, open("setup_template.py") as t:
+    setup = t.read().replace("{{ version }}", version)
+    f.write(setup)
+
+with open("README.md", 'w') as f, open("README_template.md") as t:
+    table = service_table(format='html')
+    readme = t.read().replace("{{ service_table }}", table)
+    f.write(readme)
+
+call(["python", "setup.py", "sdist", "upload"])
+call(["python", "setup.py", "develop"])
+call(["git", "commit", "-am", "Made release %s" % version])
+call(["git", "tag", version])
+call(["git", "push", "--tags"])
